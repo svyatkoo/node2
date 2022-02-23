6d8ded99eaaf496208e5d8cf30ea6888ec107f15
SviatoslavHeletii <svyatkoo@gmail.com>
2022-02-23 23:22:49 +0200

    migration

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+console.log('22');
+console.log('22');
+console.log('22');
+console.log('22');
+console.log('22');
+
+app.listen(9000, () => {
+    console.log('Server started!');
+});
